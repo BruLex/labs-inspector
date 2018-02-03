37542be9b31c1417bc21f5025bb1f801737ba8dd
Nikita_Kamak <kamak.developer@gmail.com>
2018-02-03 22:10:18 +0200

    первый фрагмент

--- LabsInspector/app/src/main/java/ua/blowball_soft/labsinspector/view/MainScreenActivity.java
+++ LabsInspector/app/src/main/java/ua/blowball_soft/labsinspector/view/MainScreenActivity.java
@@ -1,15 +1,32 @@
 package ua.blowball_soft.labsinspector.view;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import ua.blowball_soft.labsinspector.R;
 
-public class MainScreenActivity extends AppCompatActivity {
+public class MainScreenActivity extends FragmentActivity {
+
+    private FragmentManager fragmentManager;
+    private FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        fragmentManager = getSupportFragmentManager();
+
+        Fragment fragmentSubjects = fragmentManager.findFragmentById(R.id.main_screen_container);
+        if (fragmentSubjects == null) {
+            fragmentSubjects = new SubjectsFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.main_screen_container, fragmentSubjects)
+                    .commit();
+        }
     }
 }
